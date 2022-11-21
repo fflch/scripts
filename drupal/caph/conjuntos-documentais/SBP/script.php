a2fcbea41f6b525f4314b23b357a153164a470cd
sabrina <sabrinagodoy@usp.br>
2022-11-21 17:09:21 -0300

    leitura do csv

--- drupal/caph/conjuntos-documentais/SBP/script.php
+++ drupal/caph/conjuntos-documentais/SBP/script.php
@@ -2,3 +2,15 @@
 //autora: Sabrina 
 echo "iniciando script";
 
+//tarefa 1 ler o arquivo csv
+//tarefa 2 criar um array para cada linha do arquivo csv
+$arquivocsv = file_get_contents('SBP.csv');
+$lines = explode(PHP_EOL, $arquivocsv);
+
+$array = array();
+
+foreach ($lines as $line) {
+    $array[] = str_getcsv($line);
+}
+
+var_dump($array);
