d57ff74c7d7bffa4965ff88d90d257c3ad465b41
sabrina <sabrinagodoy@usp.br>
2023-01-27 09:14:14 -0300

    atualizações feitas na reunião de Drupal

--- drupal/caph/conjuntos-documentais/SBP/script.php
+++ drupal/caph/conjuntos-documentais/SBP/script.php
@@ -2,8 +2,6 @@
 /*
  * script por: Sabrina godoy.
  * 
- * 
- * 
 Suba o drupal na sua máquina local:
 No terminal, diretório projetos/drupal, digite o comando:
 ./vendor/bin/drupal serve -vvv
@@ -14,7 +12,7 @@ Coloque o título (o que de fato é o título que irá aparecer), depois o rótu
 apague o campo "body" que será criado automaticamente
 Clique em "adicionar campo"
 Selecione o tipo de campo. No caso deste script, consulte a lista abaixo, onde consta o tipo de campo, nome do campo, e o nome de máquina de cada um deles. 
-**************** É imprescindível que o nome de máqina seja igual ao da lista!!!!********************************
+**************** É imprescindível que o nome de máquina seja igual ao da lista!!!!********************************
 
 Tipo de campo     |  Nome de Máquina 
 
@@ -23,10 +21,34 @@ Tipo de campo     |  Nome de Máquina
  Texto Simples       'field_abordagem'	                 
  Texto Simples       'field_local_de_producao'           
  Texto Simples       'field_data_de_producao'            
- Texto Listagem      'field_tecnica'                     
+ Texto Listagem      'field_tecnica'  
+    adicionar os seguintes itens de listagem desta forma, em configuração do campo:
+    Datilografia|Datilografia
+    Datilografia, Impressão|Datilografia, Impressão
+    Datilografia, Impressão, Manuscrita|Datilografia, Impressão, Manuscrita
+    Datilografia, Manuscrita|Datilografia, Manuscrita
+    Fotografia analógica|Fotografia analógica
+    impressão|impressão
+    Impressão, Manuscrita|Impressão, Manuscrita
+    Manuscrita|Manuscrita
+    Manuscrita, Mimeografia|Manuscrita, Mimeografia
+    Mimeografia|Mimeografia
+    Mimeografia, Impressão|Mimeografia, Impressão 
+
  Texto Simples       'field_suporte'                     
- Texto Listagem      'field_formato'                     
- Texto Listagem      'field_cromia'                      
+ Texto Listagem      'field_formato' 
+    adicionar os seguintes itens de listagem desta forma, em configuração do campo:
+    Encadernação|Encadernação
+    Folha|Folha
+    Fotograma|Fotograma
+    Placa|Placa
+    Tira|Tira
+    
+ Texto Listagem      'field_cromia'         
+    adicionar os seguintes itens de listagem desta forma, em configuração do campo:
+    Cores|Cores
+    Preto e Branco|Preto e Branco
+     
  Texto Simples       'field_idioma'                      
  Número Inteiro      'field_numero_de_itens'              
  Número Inteiro      'field_numero_de_exemplares'         
@@ -51,22 +73,19 @@ Tipo de campo     |  Nome de Máquina
  Texto Simples       'field_descritores_4'               
  Texto Simples       'field_referencia'                  
  Texto Simples       'field_observacoes'                
-
+ Arquivo             'field_arquivo' 
 
  Depois de criar todos os tipos de contepudo no drupal, tenha este script baixado na sua máquina, e execute o seguinte comando, considerando o caminho para o seu arquivo de script
 Ex.:
 ./vendor/bin/drush php-script /home/SEU USER/-caminho restante-/script.php
-
-
-
-
- * 
  * 
  * Esse script deve ser rodado como: 
  * ./vendor/bin/drush php-script /home/sabrina/projetos/scripts/drupal/caph/conjuntos-documentais/SBP/script.php
  * 
+ * ** para apagar todos os nodes: ./vendor/bin/drupal entity:delete node --all
+ * ** para apagar todos os arquivos carregados: ./vendor/bin/drupal entity:delete file --all
  * 
- *  Fase de progresso: pesquisa de comandos para carregar os arquivos PDF's dentro do campo 'arquivo' no node criado.
+ *  Fase de progresso: pesquisa de código multivalue para o campo descritores.
  * 
  */
 use \Drupal\node\Entity\Node;
@@ -81,8 +100,6 @@ foreach ($arquivos as $arquivo) {
 }
 array_pop($arquivos_array);
 
-
-
 // lendo pdfs
 $full_path= '/home/sabrina/arquivos_SBP/Acervo Samuel Barnsley Pessoa/';
 $arquivospdf= scandir($full_path);
@@ -91,13 +108,11 @@ foreach($arquivos_array as $coluna) {
     // lendo pdfs
     $notacao = $coluna[1];
     $arquivos_relacionados = [];
-    //var_dump ($notacao);
     foreach($arquivospdf as $arquivopdf){
         if(str_contains($arquivopdf, $notacao)){
             $arquivos_relacionados[] = $arquivopdf;
         }
     }
-    //var_dump ($arquivos_relacionados);
     
 	$node = Node::create([
       	'type'          	    	        => 'caph_sbp',
@@ -130,13 +145,20 @@ foreach($arquivos_array as $coluna) {
         'field_especificacao_2'             => $coluna[25],
         'field_local_2'                     => $coluna[26],
         'field_data_ou_periodo_2'           => $coluna[27],
-        'field_descritores_1_'              => $coluna[28],
-        'field_descritores_2'               => $coluna[29],
-        'field_descritores_3'               => $coluna[30],    
-        'field_descritores_4'               => $coluna[31],
+       // 'field_descritores_1_'              => $coluna[28],
+       // 'field_descritores_2'               => $coluna[29],
+       // 'field_descritores_3'               => $coluna[30],    
+       // 'field_descritores_4'               => $coluna[31],
         'field_referencia'                  => $coluna[32],
         'field_observacoes'                 => $coluna[33],   
     ]);
+    $descritores=[
+        ["field_descritores"=> $coluna[28]],
+        ["field_descritores"=> $coluna[29]],
+        ["field_descritores"=> $coluna[30]],
+        ["field_descritores"=> $coluna[31]],
+    ];
+    $node->set('field_descritores', $descritores);
 
     $Arquivos_PDF = [];
     foreach($arquivos_relacionados as $arquivo_relacionado){
