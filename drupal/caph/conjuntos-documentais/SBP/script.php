375693ee287259e2628630b6cc03c1ed3253af50
sabrina <sabrinagodoy@usp.br>
2023-02-01 08:30:04 -0300

    adição dos campos multivalores de 'local'

--- drupal/caph/conjuntos-documentais/SBP/script.php
+++ drupal/caph/conjuntos-documentais/SBP/script.php
@@ -91,7 +91,7 @@ Ex.:
 use \Drupal\node\Entity\Node;
 use \Drupal\file\Entity\File;
 // Lendo csv
-$arquivocsv = file_get_contents('/home/acesarfs/projetos/scripts/drupal/caph/conjuntos-documentais/SBP/SBP.csv');
+$arquivocsv = file_get_contents('/home/sabrina/projetos/scripts/drupal/caph/conjuntos-documentais/SBP/SBP.csv');
 $arquivos = explode(PHP_EOL, $arquivocsv);
 $arquivos_array = array();
 
@@ -101,7 +101,7 @@ foreach ($arquivos as $arquivo) {
 array_pop($arquivos_array);
 
 // lendo pdfs
-$full_path= '/home/acesarfs/arquivos_sbp/';
+$full_path= '/home/sabrina/arquivos_SBP/SBP/';
 $arquivospdf= scandir($full_path);
 
 foreach($arquivos_array as $coluna) {
@@ -117,6 +117,7 @@ foreach($arquivos_array as $coluna) {
 	$node = Node::create([
       	'type'          	    	        => 'caph_sbp',
         'uid'                               => 1,
+        'field_acervo'                      => 'SBP',
         'title'      		                => $coluna[0],
         'field_notacao'		                => $coluna[1],
         'field_documento'	                => $coluna[2],
@@ -131,45 +132,42 @@ foreach($arquivos_array as $coluna) {
         'field_numero_de_itens'             => $coluna[11],
         'field_numero_de_exemplares'        => $coluna[12],
         'field_extensao'                    => $coluna[13],
-        'field_responsavel_1'               => $coluna[14],
-        'field_tipo_de_responsabilidade_1'  => $coluna[15],
-        'field_responsavel_2'               => $coluna[16],
-        'field_tipo_de_responsabilidade_2'  => $coluna[17],
-        'field_responsaveis_'               => $coluna[18],
-        'field_tipo_de_responsabilidade_3'  => $coluna[19],
-        'field_atividade_evento_1'          => $coluna[20],
-        'field_especificacao_1'             => $coluna[21],
-        'field_local_1'                     => $coluna[22],
-        'field_data_ou_periodo_1'           => $coluna[23],
-        'field_atividade_evento_2'          => $coluna[24],
-        'field_especificacao_2'             => $coluna[25],
-        'field_local_2'                     => $coluna[26],
-        'field_data_ou_periodo_2'           => $coluna[27],
-        'field_descritores_1'               => $coluna[28],
-        'field_descritores_2'               => $coluna[29],
-        'field_descritores_3'               => $coluna[30],    
-        'field_descritores_4'               => $coluna[31],
+        //'field_responsavel_1'               => $coluna[14],
+        //'field_tipo_de_responsabilidade_1'  => $coluna[15],
+        //'field_responsavel_2'               => $coluna[16],
+        //'field_tipo_de_responsabilidade_2'  => $coluna[17],
+        //'field_responsaveis_'               => $coluna[18],
+        //'field_tipo_de_responsabilidade_3'  => $coluna[19],
+        //'field_atividade_evento_1'          => $coluna[20],
+        //'field_especificacao_1'             => $coluna[21],
+        //'field_local_1'                     => $coluna[22],
+        //'field_data_ou_periodo_1'           => $coluna[23],
+        //'field_atividade_evento_2'          => $coluna[24],
+        //'field_especificacao_2'             => $coluna[25],
+        //'field_local_2'                     => $coluna[26],
+        //'field_data_ou_periodo_2'           => $coluna[27],
+        //'field_descritores_1'               => $coluna[28],
+        //'field_descritores_2'               => $coluna[29],
+        //'field_descritores_3'               => $coluna[30],    
+        //'field_descritores_4'               => $coluna[31],
         'field_referencia'                  => $coluna[32],
         'field_observacoes'                 => $coluna[33],   
     ]);
     
+    $Local = [];
+    $Local [] = ['value' => $coluna[22]];
+    $Local [] = ['value' => $coluna[26]];
+    $node->set('field_local', $Local);
+
     $Descritores = [];
-    $Descritores[] = [
-        'value' => $coluna[28],
-    ];
-    $Descritores[] = [
-        'value' => $coluna[29],
-    ];
-    $Descritores[] = [
-        'value' => $coluna[30],
-    ];
-    $Descritores[] = [
-        'value' => $coluna[31],
-    ];
+    $Descritores[] = ['value' => $coluna[28],];
+    $Descritores[] = ['value' => $coluna[29],];
+    $Descritores[] = ['value' => $coluna[30],];
+    $Descritores[] = ['value' => $coluna[31],];
     $node->set('field_descritores', $Descritores);
 
     $Arquivos_PDF = [];
-    foreach($arquivos_relacionados as $arquivo_relacionado){
+    foreach($arquivos_relacionados as $arquivo_relacionado) {
         $arquivo_conteudo = file_get_contents($full_path.$arquivo_relacionado);
         $file = file_save_data($arquivo_conteudo, 'public://'.$arquivo_relacionado, FILE_EXISTS_REPLACE);
         $Arquivos_PDF[] = [
@@ -178,8 +176,6 @@ foreach($arquivos_array as $coluna) {
             'title'     => 'SBP PDF',
         ];
     }
-
     $node->set('field_arquivo', $Arquivos_PDF);
-
     echo $node->save();
 }
