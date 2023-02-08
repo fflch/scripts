51b816c2edad0a2559940737fcff214be60fd983
sabrina <sabrinagodoy@usp.br>
2023-02-08 11:40:54 -0300

    alterações nas instruções de funcionamento do script

--- drupal/caph/conjuntos-documentais/SBP/script.php
+++ drupal/caph/conjuntos-documentais/SBP/script.php
@@ -7,14 +7,14 @@ No terminal, diretório projetos/drupal, digite o comando:
 ./vendor/bin/drupal serve -vvv
 Acesse o link disponibilizado no terminal, e abrirá o seu drupal local no navegador
 No drupal, acesse a ferramenta "estrutura", e selecione "tipo de conteúdo"
-clique em "adicionar tipo de conteúdo"
-Coloque o título (o que de fato é o título que irá aparecer), depois o rótulo do título
-apague o coluna$coluna "body" que será criado automaticamente
-Clique em "adicionar coluna$coluna"
-Selecione o tipo de coluna$coluna. No caso deste script, consulte a lista abaixo, onde consta o tipo de coluna$coluna, nome do coluna$coluna, e o nome de máquina de cada um deles. 
+clique em "adicionar tipo de conteúdo" 
+Coloque o título (Acervo CAPH), com nome de máquina "acervo_caph" depois o rótulo do título
+apague o campo "body" que será criado automaticamente
+Clique em "adicionar campo"
+Selecione o tipo de campo. No caso deste script, consulte a lista abaixo, onde consta o tipo de campo, nome do campo, e o nome de máquina de cada um deles. 
 **************** É imprescindível que o nome de máquina seja igual ao da lista!!!!********************************
 
-Tipo de coluna$coluna     |  Nome de Máquina 
+Tipo de campo     |  Nome de Máquina 
 
  Texto Simples       'field_notacao'		             
  Texto Simples       'field_documento'	                 
@@ -22,7 +22,7 @@ Tipo de coluna$coluna     |  Nome de Máquina
  Texto Simples       'field_local_de_producao'           
  Texto Simples       'field_data_de_producao'            
  Texto Listagem      'field_tecnica'  
-    adicionar os seguintes itens de listagem desta forma, em configuração do coluna$coluna:
+    adicionar os seguintes itens de listagem desta forma, em configuração do campo:
     Datilografia|Datilografia
     Datilografia, Impressão|Datilografia, Impressão
     Datilografia, Impressão, Manuscrita|Datilografia, Impressão, Manuscrita
@@ -37,7 +37,7 @@ Tipo de coluna$coluna     |  Nome de Máquina
 
  Texto Simples       'field_suporte'                     
  Texto Listagem      'field_formato' 
-    adicionar os seguintes itens de listagem desta forma, em configuração do coluna$coluna:
+    adicionar os seguintes itens de listagem desta forma, em configuração do campo:
     Encadernação|Encadernação
     Folha|Folha
     Fotograma|Fotograma
@@ -45,32 +45,21 @@ Tipo de coluna$coluna     |  Nome de Máquina
     Tira|Tira
     
  Texto Listagem      'field_cromia'         
-    adicionar os seguintes itens de listagem desta forma, em configuração do coluna$coluna:
+    adicionar os seguintes itens de listagem desta forma, em configuração do campo:
     Cores|Cores
     Preto e Branco|Preto e Branco
      
  Texto Simples       'field_idioma'                      
  Número Inteiro      'field_numero_de_itens'              
  Número Inteiro      'field_numero_de_exemplares'         
- Texto Simples       'field_extensao'                     
- Texto Simples       'field_responsavel_1'               
- Texto Simples       'field_tipo_de_responsabilidade_1'  
- Texto Simples       'field_responsavel_2'               
- Texto Simples       'field_tipo_de_responsabilidade_2'   
- Texto Simples       'field_responsaveis_'                
- Texto Simples       'field_tipo_de_responsabilidade_3'  
- Texto Simples       'field_atividade_evento_1'          
- Texto Simples       'field_especificacao_1'             
- Texto Simples       'field_local_1'                     
- Texto Simples       'field_data_ou_periodo_1'           
- Texto Simples       'field_atividade_evento_2'           
- Texto Simples       'field_especificacao_2'             
- Texto Simples       'field_local_2'                     
- Texto Simples       'field_data_ou_periodo_2'           
- Texto Simples       'field_descritores_1_'              
- Texto Simples       'field_descritores_2'               
- Texto Simples       'field_descritores_3'                   
- Texto Simples       'field_descritores_4'               
+ Texto Simples       'field_extensao' -----------------------------> marcar o campo como "ilimitado"                   
+ Texto Simples       'field_responsaveis'--------------------------> marcar o campo como "ilimitado"        
+ Texto Simples       'field_tipo_de_responsabilidade'--------------> marcar o campo como "ilimitado"
+ Texto Simples       'field_atividade_evento'----------------------> marcar o campo como "ilimitado"          
+ Texto Simples       'field_especificacao'-------------------------> marcar o campo como "ilimitado"             
+ Texto Simples       'field_local'---------------------------------> marcar o campo como "ilimitado"                     
+ Texto Simples       'field_data_ou_periodo'-----------------------> marcar o campo como "ilimitado"                      
+ Texto Simples       'field_descritores'---------------------------> marcar o campo como "ilimitado"                            
  Texto Simples       'field_referencia'                  
  Texto Simples       'field_observacoes'                
  Arquivo             'field_arquivo' 
@@ -85,16 +74,13 @@ Ex.:
  * ** para apagar todos os nodes: ./vendor/bin/drupal entity:delete node --all
  * ** para apagar todos os arquivos carregados: ./vendor/bin/drupal entity:delete file --all
  * 
- *  Fase de progresso: pesquisa de código multivalue para o coluna$coluna descritores.
+ *  Fase de progresso: pesquisa de código multivalue para o campo descritores.
  * 
  */
 use \Drupal\node\Entity\Node;
 use \Drupal\file\Entity\File;
-
-$home_dir = "/home/acesarfs/";
-
 // Lendo csv
-$arquivocsv = file_get_contents($home_dir . 'projetos/scripts/drupal/caph/conjuntos-documentais/SBP/SBP.csv');
+$arquivocsv = file_get_contents('/home/sabrina/projetos/scripts/drupal/caph/conjuntos-documentais/SBP/SBP.csv');
 $arquivos = explode(PHP_EOL, $arquivocsv);
 $arquivos_array = array();
 
@@ -104,7 +90,7 @@ foreach ($arquivos as $arquivo) {
 array_pop($arquivos_array);
 
 // lendo pdfs
-$full_path= $home_dir . 'arquivos_SBP/';
+$full_path= '/home/sabrina/arquivos_SBP/SBP/';
 $arquivospdf= scandir($full_path);
 
 foreach($arquivos_array as $coluna) {
@@ -118,7 +104,7 @@ foreach($arquivos_array as $coluna) {
     }
     
 	$node = Node::create([
-        'type'                              => 'acervo_caph',
+      	'type'          	    	        => 'acervo_caph',
         'uid'                               => 1,
         'field_acervo'                      => 'SBP',
         'title'      		                => $coluna[0],
@@ -134,18 +120,32 @@ foreach($arquivos_array as $coluna) {
         'field_idioma'                      => $coluna[10],
         'field_numero_de_itens'             => $coluna[11],
         'field_numero_de_exemplares'        => $coluna[12],
+        'field_extensao'                    => $coluna[13],
+        //'field_responsavel_1'               => $coluna[14],
+        //'field_tipo_de_responsabilidade_1'  => $coluna[15],
+        //'field_responsavel_2'               => $coluna[16],
+        //'field_tipo_de_responsabilidade_2'  => $coluna[17],
+        //'field_responsaveis_'               => $coluna[18],
+        //'field_tipo_de_responsabilidade_3'  => $coluna[19],
+        //'field_atividade_evento_1'          => $coluna[20],
+        //'field_especificacao_1'             => $coluna[21],
+        //'field_local_1'                     => $coluna[22],
+        //'field_data_ou_periodo_1'           => $coluna[23],
+        //'field_atividade_evento_2'          => $coluna[24],
+        //'field_especificacao_2'             => $coluna[25],
+        //'field_local_2'                     => $coluna[26],
+        //'field_data_ou_periodo_2'           => $coluna[27],
+        //'field_descritores_1'               => $coluna[28],
+        //'field_descritores_2'               => $coluna[29],
+        //'field_descritores_3'               => $coluna[30],    
+        //'field_descritores_4'               => $coluna[31],
         'field_referencia'                  => $coluna[32],
         'field_observacoes'                 => $coluna[33],   
     ]);
     
-    $Extensao   = [];
-    $Extensao[] = ['value' => $coluna[13],];
-    $Extensao[] = ['value' => $coluna[14],];
-    $node->set('field_extensao', $Extensao);
-
     $Data_Periodo   = [];
-    $Data_Periodo[] = ['value' => $coluna[26],];
-    $Data_Periodo[] = ['value' => $coluna[30],];
+    $Data_Periodo[] = ['value' => $coluna[23],];
+    $Data_Periodo[] = ['value' => $coluna[27],];
     $node->set('field_data_ou_periodo', $Data_Periodo);
 
     $Responsaveis = [];
@@ -173,7 +173,7 @@ foreach($arquivos_array as $coluna) {
     $Atividade_Evento = [];
     $Atividade_Evento[] = ['value' => $coluna[20]];
     $Atividade_Evento[] = ['value' => $coluna[24]];
-    $node->set('field_atividade_evento', $Atividade_Evento); 
+    $node->set('field_atividade_evento', $Atividade_Evento);
 
     $Descritores = [];
     $Descritores[] = ['value' => $coluna[28],];
